78e2f7e2dcdeec45bba5dcc0a887bfae52976436
rainyspringg <samuel052398@gmail.com>
2026-03-29 14:09:26 +0800

    Added MathCalculator

--- MathApp/MathCalculator.cs
+++ MathApp/MathCalculator.cs
@@ -1,1 +1,17 @@
-﻿
+﻿namespace MathApp;
+
+public class MathCalculator
+{
+    public double Number { get; set; }
+
+    public MathCalculator(double number)
+    {
+        Number = number;
+    }
+
+    // The function we will be testing
+    public double GetSquare()
+    {
+        return Number * Number;
+    }
+}
---
+++ MathApp/UnitTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace MathApp;
+
+internal class UnitTest
+{
+    public void TestGetSquare()
+    {
+        // 1. Setup: Create a calculator and give it the number 5
+        MathCalculator calc = new MathCalculator(5);
+
+        // 2. Execute: 5 squared should be 25
+        double result = calc.GetSquare();
+
+        // 3. Evaluate
+        if (result == 25)
+        {
+            Debug.WriteLine("[TEST] GetSquare method PASSED.");
+        }
+        else
+        {
+            Debug.WriteLine($"[TEST] GetSquare method FAILED. Expected 25, got {result}");
+        }
+    }
+}
